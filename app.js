a8d43ecf02f56781bbfcbb6803feee21ef22da6d
Hamit Okuyucular <111137936+hamitokuyucular@users.noreply.github.com>
2025-09-03 01:40:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,157 +1,331 @@
 // Haritayı oluştur
 var map = L.map('map', { zoomControl:true }).setView([39.0, 35.0], 6);
-
+    
  // Altlık harita ekle
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Çizim özelliklerini belirle
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
-  
-var drawControl = new L.Control.Draw({
-  edit: {
-      featureGroup: drawnItems // düzenleme yapılacak layer
-  },
-  draw: {
-    polygon: true,
-    polyline: true,
-    rectangle: true,
-    circle: true,
-    marker: true
-  }
-});
-
-map.addControl(drawControl);
-map.on(L.Draw.Event.CREATED, function (event) {
-    var layer = event.layer;
-    drawnItems.addLayer(layer); // çizilen objeyi haritaya ekle
-    console.log(layer.toGeoJSON()); // GeoJSON formatında bilgiyi görebilirsin
-});
-
 // Layer control nesnesi
 var baseMaps = {"OSM": osm};
 var overlays = {};
 var layerControl = L.control.layers(baseMaps, overlays).addTo(map);
 
-// Listeye ekleme fonksiyonu
-function addDataToList(id, name) {
-  const dataElement = document.createElement("div");
-  dataElement.innerHTML = `
-  <div class="layer-name">
-    ${name}
-  </div>
-  <span>
-    <button onclick="zoomToLayer('${id}')" class="btn btn-primary btn-group">
-      Zoom
-    </button>
-    <button onclick="removeLayer('${id}', this)" class="btn btn-danger btn-group">
-      Sil
-    </button>
-  </span>
-  `;
-  document.getElementById("layer-list").appendChild(dataElement)
-};
+const btnGeojson = document.getElementById("btn-geojson");
+const btnShapefile = document.getElementById("btn-shp");
+const fileInput = document.getElementById("file-input");
 
-// katman listesinde ilgili katmana zoom
-function zoomToLayer(id){
-  const geoJsonLayer = dataList[id];
-  if (geoJsonLayer){
-    map.fitBounds(geoJsonLayer.getBounds());
+btnGeojson.addEventListener('click', () => {
+  fileInput.accept= ".json, .geojson";
+  fileInput.click();
+});
+
+btnShapefile.addEventListener('click', () => {
+  fileInput.accept= ".zip";
+  fileInput.click();
+});
+
+fileInput.addEventListener('change', fileLoader);
+
+function fileLoader(e){
+  const file = e.target.files[0];
+  if (!file) return;
+
+  const fileName = file.name;
+  const fileExtension = fileName.split(".").pop().toLowerCase();
+  
+  if (fileExtension == "json" || fileExtension== "geojson") {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const geojson = JSON.parse(e.target.result);
+      addGeojsonToMap(geojson, file.name);
+    }
+    reader.readAsText(file);
+    
+  } else if (fileExtension == "zip") {
+    async function loadShapefile(file) {
+      const arrayBuffer = await file.arrayBuffer();
+      const geojson = await shp(arrayBuffer);
+      addGeojsonToMap(geojson, file.name);
+    }
+    loadShapefile(file)
   }
-};
+}
 
-// katman listesinde ilgili katmanı sil
-function removeLayer(id, btn){
-  const geoJsonLayer = dataList[id];
-  if (geoJsonLayer) {
-    map.removeLayer(geoJsonLayer);
-    delete dataList[id];
-    layerControl.removeLayer(geoJsonLayer);
-    btn.closest("div").remove();
-  }
-};
+let savedLayers = [];
 
-// Popup fonksiyonu
-function createPopup(feature, layer) {
-  let props = feature.properties;
-  let content = "";
-  for (let key in props) {
-    content += key + ": " + props[key] + "<br>";
-  }
-  layer.bindPopup(content);
-};
+function addGeojsonToMap(geojson, name){
+  const layer = L.geoJSON(geojson, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius:6,
+        color: 'black',
+        fillColor: 'blue',
+        weight: 1,
+        fillOpacity: 0.7
+      });
+    },
+    onEachFeature: function (feature, layer) {
+      if (feature.properties) {
+        let props = feature.properties;
+        let content = "";
+        for (let key in props) {
+          content += key + ": " + props[key] + "<br>";
+        }
+        layer.bindPopup(content);
+      }
 
-// Tıklandığında koordinat bilgisi versin
-// var popup = L.popup();
-// function onMapClick(e) {
-//   popup
-//     .setLatLng(e.latlng)
-//     .setContent("Coordinates: " + e.latlng.toString())
-//     .openOn(map);
-// }
-// map.on('click', onMapClick);
+      layer.on('click', function(e) {
+        if (analysisMode.checked) {
+          selectFeatureForAnalysis(layer, feature);
+        }
+      });
+    }
+  }).addTo(map);
 
-
-dataList = {}; // yüklenecek datanın id'si tutulacak
-// Dosya yükleme
-async function addData(data, name, type) {
-  let layer;
-  if (type == "geojson" || type == "shapefile") {
-    layer = L.geoJSON(data, {
-        onEachFeature: createPopup
-    }).addTo(map);
-    map.fitBounds(layer.getBounds());
-  }
-  else if (type == "raster"){
-    layer = new GeoRasterLayer({
-      georaster: data,
-      opacity: 0.7,
-      resolution: 256
-    }).addTo(map);
-    map.fitBounds(layer.getBounds());
+  const layerInfo = {
+    name: name,
+    layer: layer,
+    geojson: geojson
   };
+
+  map.fitBounds(layer.getBounds());  
   overlays[name] = layer;
   layerControl.addOverlay(layer, name);
-  const dataId = "data-" + Date.now();
-  dataList[dataId] = layer;
-  addDataToList(dataId, name);
-};
 
-// GeoJSON Seçme
-document.getElementById("btn-geojson").onclick = () =>
-  document.getElementById("geojsonFile").click();
-document.getElementById("geojsonFile").addEventListener('change', async (e) => {
-  const file = e.target.files[0];
-  if (!file) return;
-  const text = await file.text();
-  const geojson = JSON.parse(text);
-  await addData(geojson, file.name, "geojson");
-  e.target.value = '';
-});
+  savedLayers.push(layerInfo);
+  aditLayerListDiv ()
+  editTargetLayerSelect();
+}
 
-// Shapefile Seçme
-document.getElementById("btn-shp").onclick = () =>
-  document.getElementById("zipFile").click();
-document.getElementById("zipFile").addEventListener('change', async (e) => {
-  const file = e.target.files[0];
-  if (!file) return;
-  const zip = await file.arrayBuffer();
-  const shape = await shp(zip);
-  await addData(shape, file.name, "shapefile");
-  e.target.value = '';
-});
 
-// Raster Seçme
-document.getElementById("btn-raster").onclick = () =>
-  document.getElementById("rasterFile").click();
-document.getElementById("rasterFile").addEventListener('change', async (e) => {
-  const file = e.target.files[0];
-  if (!file) return;
-  const raster = await file.arrayBuffer();
-  const georaster = await parseGeoraster(raster);
-  await addData(georaster, file.name, "raster");
-  e.target.value = '';
-});
+const layerList = document.getElementById("layer-list")
+
+function aditLayerListDiv () {
+  layerList.innerHTML = '';
+
+  if (savedLayers.length === 0) {
+    layerList.innerHTML = `
+      <div class="instructions">
+        <p>Katman listesi boş. Görüntülemek veya analiz yapmak için veri yükeyin.</p>
+      </div>
+    `;
+    return;
+  }
+
+  savedLayers.forEach((layerInfo, index) => {
+    const layerItem = document.createElement("div");
+    
+    layerItem.innerHTML = `
+      <div class="layer-name">${layerInfo.name}</div>
+      <div class="layer-actions">
+          <button class="zoom-layer" data-index="${index}">Zoom</button>
+          <button class="remove-layer" data-index="${index}">Kaldır</button>
+      </div>
+    `;
+    layerList.appendChild(layerItem);
+  });
+
+  document.querySelectorAll(".remove-layer").forEach(button => {
+    button.addEventListener('click', function(){
+      const index = parseInt(this.getAttribute("data-index"));
+      removeLayer(index)
+    });
+  });
+
+  document.querySelectorAll(".zoom-layer").forEach(button => {
+    button.addEventListener('click', function() {
+      const index = parseInt(this.getAttribute("data-index"));
+      zoomToLayer(index);
+    });
+  });
+}
+
+function removeLayer(index) {
+  const layerInfo = savedLayers[index];
+  map.removeLayer(layerInfo.layer);
+  layerControl.removeLayer(layerInfo.layer);
+  savedLayers.splice(index,1);
+
+  aditLayerListDiv();
+  editTargetLayerSelect();
+}
+
+function zoomToLayer(index) {
+  const layerInfo = savedLayers[index];
+  map.fitBounds(layerInfo.layer.getBounds());
+}
+
+const analysisMode = document.getElementById("analysis-mode")
+const infoMode = document.getElementById("info-mode")
+const analysisDiv = document.getElementById("div-analysis")
+
+analysisMode.addEventListener('change', Modes)
+infoMode.addEventListener('change', Modes)
+
+let selectedLayer = null;
+let selectedFeature = null;
+
+function selectFeatureForAnalysis(layer, feature){
+  selectedLayer = layer;
+  selectedFeature = feature;
+
+  selectedFeatureView(layer);
+  map.fitBounds(layer.getBounds());
+}
+
+function Modes(){
+  if (analysisMode.checked) {
+    analysisDiv.classList.remove('hidden');
+
+    if (selectedLayer) {
+      selectedLayer = null
+      selectedFeature = null
+    }
+  } else {
+    analysisDiv.classList.add('hidden')
+  }
+}
+
+const targetLayerSelect = document.getElementById("target-layer")
+
+function editTargetLayerSelect() {
+  targetLayerSelect.innerHTML = '<option value="">Katman seçin</option>';
+
+  savedLayers.forEach(layerInfo => {
+    const option = document.createElement('option');
+    option.value = layerInfo.name;
+    option.textContent = layerInfo.name;
+    targetLayerSelect.appendChild(option);
+  });
+}
+
+
+function selectedFeatureView(layer) {
+  if (layer.feature && layer.feature.geometry) {
+    const geometryType = layer.feature.geometry.type;
+    if (geometryType == 'Point' || geometryType == 'MultiPoint') {
+      layer.setStyle({
+        radius: 6,
+        weight: 1,
+        color: 'black',
+        fillColor: 'red',
+        fillOpacity: 0.7
+      });
+    } else {
+      layer.setStyle({
+        color: 'red',
+        weight: 1
+      })
+    }
+  }
+}
+
+const intersectBtn = document.getElementById('intersect-btn');
+const withinBtn = document.getElementById('within-btn');
+
+intersectBtn.addEventListener('click', () => executeAnalysis('intersect'));
+withinBtn.addEventListener('click', () => executeAnalysis('within'));
+
+function executeAnalysis(analysisType) {
+  const targetLayerName = targetLayerSelect.value;
+  const targetLayerInfo = savedLayers.find(
+    layer => layer.name == targetLayerName
+  );
+
+  let resultsFeatures = []
+
+  const selectedFeature_Truf = turf.feature(
+    selectedFeature.geometry,
+    selectedFeature.properties
+  );
+
+  targetLayerInfo.geojson.features.forEach(targetFeature => {
+    const targetFeature_Turf = turf.feature(
+      targetFeature.geometry,
+      targetFeature.properties
+    );
+
+    if (analysisType == "within") {
+      if (turf.booleanWithin(targetFeature_Turf, selectedFeature_Truf)) {
+        resultsFeatures.push(targetFeature);
+      }
+    } else if (analysisType == "intersect") {
+      if (turf.booleanIntersects(targetFeature_Turf, selectedFeature_Truf)) {
+        resultsFeatures.push(targetFeature);
+      }
+    }
+  });
+  analysisResults(resultsFeatures, analysisType, targetLayerName);
+}
+
+const resultsDiv = document.getElementById("result-div")
+const results = document.getElementById("results")
+const resultsDownloadBtn = document.getElementById("download-results")
+
+// show results
+function analysisResults(features, analysisType, targetLayerName) {
+  if (features.length === 0) {
+    results.innerHTML = `<p>Sonuç bulunamadı.</p>`;
+    resultsDownloadBtn.classList.add('hidden');
+    resultsDiv.classList.remove('hidden');
+    return;
+  }
+
+  analysisResultsAddToMap = L.geoJSON({
+    type: 'FeatureCollection',
+    features: features
+  }, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: 8,
+        weight: 1,
+        color: 'black',
+        fillColor: 'green',
+        fillOpacity: 0.7
+      });
+    },
+    style: {
+      color: 'green',
+      weight: 3
+    }
+  }).addTo(map);
+
+  results.innerHTML = `
+  <p><strong>Analiz Türü:</strong> ${analysisType}</p>
+  <p><strong>Hedef Katman:</strong> ${targetLayerName}</p>
+  <p><strong>Bulunan Özellik Sayısı:</strong> ${features.length}</p>
+  `;
+
+  if (features.length > 0) {
+    results.innerHTML += '<p><strong>Özellikler:</strong></p>';
+
+    const resultsTable = document.createElement("table")
+    const rowTitles = document.createElement("tr");
+
+    const properties = Object.keys(features[0].properties);
+    properties.forEach(prop => {
+      const th = document.createElement("th")
+      th.textContent = prop;
+      rowTitles.appendChild(th);
+    });
+    resultsTable.appendChild(rowTitles);
+
+    features.slice(0,5).forEach(feature => {
+      const row = document.createElement("tr");
+      properties.forEach(prop => {
+        const td = document.createElement("td");
+        td.textContent = feature.properties[prop] !== null ? feature.properties[prop] : 'null';
+        row.appendChild(td);
+      });
+      resultsTable.appendChild(row);
+    });
+    results.appendChild(resultsTable);
+
+    resultsDownloadBtn.classList.remove('hidden');
+  } else {
+    resultsDownloadBtn.classList.add('hidden');
+  }
+  resultsDiv.classList.remove('hidden');
+}
